c3f0329176b768c4458478e4ad54abe14bbfbb7b
lucamjdimarco <lucadm24@gmail.com>
2024-05-03 11:41:43 +0200

    Inserita la nostra implementazione nei file tc.c e tc.bpf.c

--- src/c/tc.bpf.c
+++ src/c/tc.bpf.c
@@ -1,348 +1,57 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
 /* Copyright (c) 2022 Hengqi Chen */
-
-#if 1
 #include <vmlinux.h>
-#include <errno.h>
-
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-
-#include "common.h"
-
-#define TC_ACT_OK		0
-#define TC_ACT_SHOT		2
-
-/*   */
-#define CLOCK_BOOTTIME		7
-
-#else
-#include <time.h>
-#include <errno.h>
-
-#include <linux/bpf.h>
-#include <linux/ip.h>
-#include <linux/if_ether.h>
-#include <linux/pkt_cls.h>
-
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_endian.h>
 
-#include "common.h"
+#define TC_ACT_OK 0
+#define ETH_P_IP  0x0800 /* Internet Protocol packet	*/
+#define IPPROTO_TCP 6   /* TCP protocol */
+#define IPPROTO_UDP 17  /* UDP protocol */
+#define IPPROTO_ICMP 1  /* ICMP protocol */
+#define MAX_ENTRIES 256
+#define MAX_COUNTER 4294967295 /* 2 ^ 32 */
 
-#endif
 
-#define ETH_P_IP	0x0800 /* Internet Protocol packet */
-#define ETH_P_IPV6	0x86DD /* Internet Protocol V6 packet */
 
-#ifndef memset
-# define memset(dest, chr, n)   __builtin_memset((dest), (chr), (n))
-#endif
+struct packet_info {
+	__u32 src_ip;
+	__u32 dst_ip;
+	__u16 src_port;
+	__u16 dst_port;
+	__u8 protocol;
+};
 
-#ifndef memcpy
-# define memcpy(dest, src, n)   __builtin_memcpy((dest), (src), (n))
-#endif
+struct value_packet {
 
-#ifndef memmove
-# define memmove(dest, src, n)  __builtin_memmove((dest), (src), (n))
-#endif
+    //sizeof(bpf_spin_lock) = 4 byte
+    struct bpf_spin_lock lock;
+    __u32 counter; 
+};
 
-/*
- * Note: including linux/compiler.h or linux/kernel.h for the macros below
- * conflicts with vmlinux.h include in BPF files, so we define them here.
- *
- * Following functions are taken from kernel sources and
- * break aliasing rules in their original form.
- *
- * While kernel is compiled with -fno-strict-aliasing,
- * perf uses -Wstrict-aliasing=3 which makes build fail
- * under gcc 4.4.
- *
- * Using extra __may_alias__ type to allow aliasing
- * in this case.
- */
-typedef __u8  __attribute__((__may_alias__))  __u8_alias_t;
-typedef __u16 __attribute__((__may_alias__)) __u16_alias_t;
-typedef __u32 __attribute__((__may_alias__)) __u32_alias_t;
-typedef __u64 __attribute__((__may_alias__)) __u64_alias_t;
-
-static __always_inline void __read_once_size(const volatile void *p, void *res, int size)
-{
-	switch (size) {
-	case 1: *(__u8_alias_t  *) res = *(volatile __u8_alias_t  *) p; break;
-	case 2: *(__u16_alias_t *) res = *(volatile __u16_alias_t *) p; break;
-	case 4: *(__u32_alias_t *) res = *(volatile __u32_alias_t *) p; break;
-	case 8: *(__u64_alias_t *) res = *(volatile __u64_alias_t *) p; break;
-	default:
-		asm volatile ("" : : : "memory");
-		__builtin_memcpy((void *)res, (const void *)p, size);
-		asm volatile ("" : : : "memory");
-	}
-}
-
-#define READ_ONCE(x)					\
-({							\
-	union { typeof(x) __val; char __c[1]; } __u =	\
-		{ .__c = { 0 } };			\
-	__read_once_size(&(x), __u.__c, sizeof(x));	\
-	__u.__val;					\
-})
-
-#if 0
-static __always_inline void __write_once_size(volatile void *p, void *res, int size)
-{
-	switch (size) {
-	case 1: *(volatile  __u8_alias_t *) p = *(__u8_alias_t  *) res; break;
-	case 2: *(volatile __u16_alias_t *) p = *(__u16_alias_t *) res; break;
-	case 4: *(volatile __u32_alias_t *) p = *(__u32_alias_t *) res; break;
-	case 8: *(volatile __u64_alias_t *) p = *(__u64_alias_t *) res; break;
-	default:
-		asm volatile ("" : : : "memory");
-		__builtin_memcpy((void *)p, (const void *)res, size);
-		asm volatile ("" : : : "memory");
-	}
-}
-
-#define WRITE_ONCE(x, val)				\
-({							\
-	union { typeof(x) __val; char __c[1]; } __u =	\
-		{ .__val = (val) }; 			\
-	__write_once_size(&(x), __u.__c, sizeof(x));	\
-	__u.__val;					\
-})
-#endif
-
-/* in KB */
-#define RB_SIZE (254 * 1024)
 struct {
-	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, RB_SIZE);
-} rbuf SEC(".maps");
+	 __uint(type, BPF_MAP_TYPE_HASH);
+	 __uint(max_entries, MAX_ENTRIES);
+	 __type(key, struct packet_info);
+	 __type(value, struct value_packet);
+} my_map SEC(".maps");
 
-struct hmap_elem_ab {
-	__u64 a;
-	__u64 b;
-};
-
-struct hmap_elem {
-	__u64 init;
-	__u64 counter;
-	/* protected by bpf spin lock */
-	struct hmap_elem_ab ab;
-
-	struct bpf_timer timer;
-	struct bpf_spin_lock lock;
-	/* NOTE: that sizeof(lock) is 32 bit wide. If we need to move it in the
-	 * structure we should fill the holes manually to avoid the verfier
-	 * complains about the structure layout, e.g. strange errors such as
-	 * missing timer definition in map during timer set up, etc.
-	 */
-	__u32 pad0;
-};
-
-#define HMAP_MAX_ENTRIES 256
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, HMAP_MAX_ENTRIES);
-	__type(key, __u32);
-	__type(value, struct hmap_elem);
-} hmap SEC(".maps");
-
-enum {
-	KEY_PROTO_UNDEF	= 0,
-	KEY_PROTO_IP	= 1,
-};
-
-struct test_hmap_elem {
-	__u32 hit_counter;
-};
-
-/* test map used for checking syscal bpf invokation from userland */
-#define TEST_HMAP_MAX_ENTRIES 1024
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, TEST_HMAP_MAX_ENTRIES);
-	__type(key, __u32);
-	__type(value, struct test_hmap_elem);
-} test_hmap SEC(".maps");
-
-static __always_inline void
-update_map_block_protected(struct hmap_elem *helem,
-			   struct hmap_elem_ab *copy_ab)
-{
-	struct hmap_elem_ab *ab = &helem->ab;
-
-	bpf_spin_lock(&helem->lock);
-
-	/* some trival ops that must be considered as a whole */
-	++ab->a;
-	ab->b = ab->a << 1;
-
-	if (copy_ab)
-		memcpy(copy_ab, ab, sizeof(*copy_ab));
-
-	bpf_spin_unlock(&helem->lock);
-}
-
-#if ALWAYS_PRINT_UPDATE
-static __always_inline void print_hmap_elem_ab(struct hmap_elem_ab *ab)
-{
-	bpf_printk("hmap_elem_ab (a=%llu, b=%llu)", ab->a, ab->b);
-}
-#endif
-
-static __always_inline int update_test_hmap(struct hmap_elem *helem)
-{
-	struct hmap_elem_ab copy_ab;
-	struct test_hmap_elem *val;
-	__u64 counter;
-	__u32 key;
-
-	update_map_block_protected(helem, &copy_ab);
-#if ALWAYS_PRINT_UPDATE
-	print_hmap_elem_ab(&copy_ab);
-#endif
-
-	/* READ_ONCE as counter could be modified by another cpu */
-	counter = READ_ONCE(helem->counter);
-	key = counter & (TEST_HMAP_MAX_ENTRIES - 1);
-
-	val = bpf_map_lookup_elem(&test_hmap, &key);
-	if (!val) {
-		struct test_hmap_elem init = {
-			.hit_counter = 1,
-		};
-
-		return bpf_map_update_elem(&test_hmap, &key, &init, 0);
-	}
-
-	__sync_fetch_and_add(&val->hit_counter, 1);
-
-	return 0;
-}
-
-#define BPF_TIMER_TIMEOUT ((__u64)5000000000)
-
-static int timer_cb(void *map, int *key, struct hmap_elem *helem)
-{
-	struct hmap_elem_ab copy_ab;
-	struct event *e;
-
-	update_map_block_protected(helem, &copy_ab);
-
-	bpf_printk("timer_cb called, key=%d, helem->counter=%llu, (a=%llu, b=%llu)",
-		   *key, helem->counter, copy_ab.a, copy_ab.b);
-
-	/* let's send data to userspace using ring buffer */
-	e = bpf_ringbuf_reserve(&rbuf, sizeof(*e), 0);
-	if (!e)
-		/* no space left on ring buffer */
-		goto out;
-
-	/* fill the event */
-	e->ts = bpf_ktime_get_ns();
-	/* TODO: harcoded at the moment */
-	e->flowid = 1;
-	e->counter = copy_ab.a;
-
-	bpf_ringbuf_submit(e, 0);
-out:
-	return 0;
-}
-
-static __always_inline int timer_init(void *map, struct hmap_elem *helem)
-{
-	int rc;
-
-	rc = bpf_timer_init(&helem->timer, map, CLOCK_BOOTTIME);
-	if (rc) {
-		bpf_printk("bpf_timer_init() rc=%d", rc);
-		return rc;
-	}
-
-	rc = bpf_timer_set_callback(&helem->timer, timer_cb);
-	if (rc) {
-		bpf_printk("bpf_timer_set_callback() rc=%d", rc);
-		return rc;
-	}
-
-	return 0;
-}
-
-/*   */
-static __always_inline
-struct hmap_elem *hmap_elem_init_or_get(void *map, __u32 *key)
-{
-	struct hmap_elem *val, init;
-	int rc;
-
-	val = bpf_map_lookup_elem(map, key);
-	if (val)
-		return val;
-
-	/* we need to initialize the element */
-	memset(&init, 0, sizeof(init));
-	init.counter = 0;
-
-	/* note that updating an hashtable element is an atomic op */
-	rc = bpf_map_update_elem(map, key, &init, BPF_NOEXIST);
-	if (rc) {
-		if (rc == -EEXIST)
-			/* another cpu has just created the entry, give up*/
-			goto lookup;
-
-		/* other errors are not tolerated */
-		return NULL;
-	}
-
-lookup:
-	val = bpf_map_lookup_elem(map, key);
-	if (!val)
-		return NULL;
-
-	/* see https://reviews.llvm.org/D72184 */
-	if (__sync_lock_test_and_set(&val->init, 1))
-		/* already initialized */
-		return val;
-
-	/* only a single CPU can be here */
-
-	/* we want to initialize a timer only once.
-	 * A timer needs to be defined inside a map element which is already
-	 * stored in the map. For this reason, we cannot  use a
-	 * publish/subscribe approach - e.g. create a map element, initialize
-	 * the timer within it and finally update the map with that element.
-	 * Publish allows cpus to see the whole map element fully initialized
-	 * or not.
-	 *
-	 * Our approach in this case, is to allow *only* a  CPU to initialized
-	 * the timer when a new map element is createde and pushed into the
-	 * map. However, in the mean while the CPU is taking the element lock
-	 * and initialize the timer, another CPU could reference to that
-	 * element finding that timer is not still initialized. We admit this
-	 * corner case as it could happen only the first time a new map element
-	 * is created inside the map.
-	 */
-	rc = timer_init(map, val);
-	if (rc)
-		return NULL;
-
-	return val;
-}
-
-#define skb_data(addr)	((void *)(__u64)(addr))
 
 SEC("tc")
 int tc_ingress(struct __sk_buff *ctx)
 {
-	void *data_end = skb_data(ctx->data_end);
-	void *data = skb_data(ctx->data);
-	struct hmap_elem *helem;
+	void *data_end = (void *)(__u64)ctx->data_end;
+	void *data = (void *)(__u64)ctx->data;
 	struct ethhdr *l2;
 	struct iphdr *l3;
-	__u32 key;
-	int rc;
+
+    struct packet_info new_info = {};
+
+    struct value_packet *packet;
+    int ret, cpu;
+
 
 	if (ctx->protocol != bpf_htons(ETH_P_IP))
 		return TC_ACT_OK;
@@ -355,52 +64,79 @@ int tc_ingress(struct __sk_buff *ctx)
 	if ((void *)(l3 + 1) > data_end)
 		return TC_ACT_OK;
 
-	/* here only if we are handling IP packets */
+    __u8 protocol = l3->protocol;
+    
+    new_info.src_ip = l3->saddr,
+    new_info.dst_ip = l3->daddr,
+    new_info.src_port = 0,
+    new_info.dst_port = 0,
+    new_info.protocol = l3->protocol;
+	
 
-	bpf_printk("! Got IP packet: tot_len: %d, ttl: %d",
-		   bpf_ntohs(l3->tot_len), l3->ttl);
+    if (protocol == IPPROTO_TCP) {
+        struct tcphdr *tcph = (struct tcphdr *)(l3 + 1);
+        if ((void *)(tcph + 1) > data_end)
+            return TC_ACT_OK;
 
-	key = KEY_PROTO_IP;
-	helem = hmap_elem_init_or_get(&hmap, &key);
-	if (!helem) {
-		bpf_printk("invalid hmap_elem object");
-		goto drop;
-	}
+        new_info.src_port = bpf_ntohs(tcph->source);
+        new_info.dst_port = bpf_ntohs(tcph->dest);
 
-	/* atomic add (increment) */
-	__sync_fetch_and_add(&helem->counter, 1);
+    } else if (protocol == IPPROTO_UDP) {
+        struct udphdr *udph = (struct udphdr *)(l3 + 1);
+        if ((void *)(udph + 1) > data_end)
+            return TC_ACT_OK;
 
-	/* start timer */
-	rc = bpf_timer_start(&helem->timer, BPF_TIMER_TIMEOUT, 0);
-	if (rc) {
-		if (rc == -EINVAL) {
-			/* This use case can be tolerated, as it is very rare.
-			 * If we have arrived at this point, it indicates that
-			 * two packets are being processed simultaneously on
-			 * two different CPUs, and both are attempting to
-			 * initialize the corresponding element. However, the
-			 * operation is intended to be performed by only one
-			 * CPU.
-			 * Therefore, it is possible that while one CPU is
-			 * initializing the timer, the completed operation may
-			 * not yet be visible on the current CPU.
-			 */
-			bpf_printk("bpf_timer not started yet.");
-			goto out;
+        new_info.src_port = bpf_ntohs(udph->source);
+        new_info.dst_port = bpf_ntohs(udph->dest);
+
+    }
+    if(protocol == IPPROTO_ICMP) {
+        struct icmphdr *icmph = (struct icmphdr *)(l3 + 1);
+        if ((void *)(icmph + 1) > data_end)
+            return TC_ACT_OK;
+
+    }
+
+    /* --- IMPLEMENTAZIONE DELLA HASH MAP ---*/
+
+    
+	cpu = bpf_get_smp_processor_id();
+    bpf_printk("Il codice BPF sta eseguendo sulla CPU %u\n", cpu);
+	packet = bpf_map_lookup_elem(&my_map, &new_info);
+
+	if(!packet) {
+		struct value_packet new_value = {
+			.counter = 1
+		};
+
+        bpf_printk("Create new item with counter 1\n");
+
+
+		bpf_printk("-----------------------------------------------------");
+		ret = bpf_map_update_elem(&my_map, &new_info, &new_value, BPF_ANY);
+        if (ret) {
+			bpf_printk("Failed to insert new item\n");
+			return TC_ACT_OK;
 		}
+	} else {
 
-		bpf_printk("bpf_timer_start() rc=%d", rc);
-		goto drop;
+        bpf_printk("Found item\n");
+
+        if (packet->counter < MAX_COUNTER) {
+            bpf_spin_lock(&packet->lock);
+            packet->counter += 1;
+            bpf_spin_unlock(&packet->lock);
+        } else {
+            bpf_printk("Counter is at maximum value\n");
+        }
+
+        bpf_printk("Counter: %u\n", packet->counter);
+
+        bpf_printk("-----------------------------------------------------");
+
 	}
-out:
-	/* fill test map used for retrieving hash entries from userland */
-	update_test_hmap(helem);
 
 	return TC_ACT_OK;
-
-drop:
-	bpf_printk("Dropped IP packet");
-	return TC_ACT_SHOT;
 }
 
 char __license[] SEC("license") = "GPL";
--- src/c/tc.c
+++ src/c/tc.c
@@ -2,11 +2,35 @@
 /* Copyright (c) 2022 Hengqi Chen */
 #include <signal.h>
 #include <unistd.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <bpf/bpf_endian.h>
+#include <bpf/libbpf.h>
 #include "tc.skel.h"
 
-#include "common.h"
+/* PER MODIFICARE L'INTERFACCIA IN CUI INSERIRE IL TRACING BPF BISOGNA INSERIRE IL SUO INDICE (CHE È POSSIBILE DETERMINARE CON IL COMANDO IP LINK SHOW). SUCCESSIVAMENTE MODIFICARE LA LINEA DI CODICE DECLARE_LIBBPF_OPTS. Con indice = 1 lavori sulla LO, mentre con indice 2 lavori sulla ENP0S3  */
+
+/* 1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+   2: enp0s3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP mode DEFAULT group default qlen 1000
+    link/ether 08:00:27:70:b6:6f brd ff:ff:ff:ff:ff:ff
+*/
 
 #define LO_IFINDEX 1
+#define ENP0S3_IFINDEX 2
+
+struct packet_info {
+	__u32 src_ip;
+	__u32 dst_ip;
+	__u16 src_port;
+	__u16 dst_port;
+	__u8 protocol;
+};
+
+struct value_packet {
+    struct bpf_spin_lock lock;
+    __u32 counter;
+};
 
 static volatile sig_atomic_t exiting = 0;
 
@@ -20,25 +44,20 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va
 	return vfprintf(stderr, format, args);
 }
 
-int handle_event(void *ctx, void *data, size_t data_sz)
-{
-	const struct event *e = data;
-
-	printf("ts: %llu, flowid: %llu, a: %llu\n",
-	       e->ts, e->flowid, e->counter);
-
-	return 0;
-}
-
 int main(int argc, char **argv)
 {
-	DECLARE_LIBBPF_OPTS(bpf_tc_hook, tc_hook, .ifindex = LO_IFINDEX,
+	//DECLARE_LIBBPF_OPTS(bpf_tc_hook, tc_hook, .ifindex = LO_IFINDEX,
+			    //.attach_point = BPF_TC_INGRESS);
+	DECLARE_LIBBPF_OPTS(bpf_tc_hook, tc_hook, .ifindex = ENP0S3_IFINDEX,
 			    .attach_point = BPF_TC_INGRESS);
 	DECLARE_LIBBPF_OPTS(bpf_tc_opts, tc_opts, .handle = 1, .priority = 1);
 	bool hook_created = false;
-	struct ring_buffer *rbuf;
 	struct tc_bpf *skel;
 	int err;
+
+	struct packet_info key;
+    struct value_packet value;
+	int map_fd;
 
 	libbpf_set_print(libbpf_print_fn);
 
@@ -75,29 +94,64 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	/* ring buffer */
-	rbuf = ring_buffer__new(bpf_map__fd(skel->maps.rbuf), handle_event,
-				NULL, NULL);
-	if (!rbuf) {
-		err = -1;
-		fprintf(stderr, "Failed to create ring buffer\n");
-		goto cleanup;
-	}
-
 	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
 	       "to see output of the BPF program.\n");
 
-	while (!exiting) {
-		err = ring_buffer__poll(rbuf, 100 /* timeout, ms */);
-		if (err == -EINTR) {
-			err = 0;
-			break;
-		}
+	map_fd = bpf_map__fd(skel->maps.my_map);
+	if (map_fd < 0) {
+		fprintf(stderr, "Failed to get map file descriptor\n");
+		return 1;
+	}
 
-		if (err < 0) {
-			printf("Error polling ring buffer: %d\n", err);
-			break;
-		}
+	while (!exiting) {
+		/*fprintf(stderr, ".");
+		sleep(1);*/
+
+		int counter = 0;
+		
+		memset(&key, 0, sizeof(key));
+		
+        
+        while (bpf_map_get_next_key(map_fd, &key, &key) == 0) {
+
+			counter++;
+			
+            
+            int ret = bpf_map_lookup_elem(map_fd, &key, &value);
+            if (ret) {
+                fprintf(stderr, "Failed to lookup map element\n");
+                return 1;
+            }
+
+			__u8 byte1 = key.src_ip & 0xFF;
+			__u8 byte2 = (key.src_ip >> 8) & 0xFF;
+			__u8 byte3 = (key.src_ip >> 16) & 0xFF;
+			__u8 byte4 = (key.src_ip >> 24) & 0xFF;
+
+			printf("---------------\n");
+            printf("Key: Source IP: %u.%u.%u.%u\n", byte1, byte2, byte3, byte4);
+
+			byte1 = key.dst_ip & 0xFF;
+			byte2 = (key.dst_ip >> 8) & 0xFF;
+			byte3 = (key.dst_ip >> 16) & 0xFF;
+			byte4 = (key.dst_ip >> 24) & 0xFF;
+			printf("Key: Destination IP: %u.%u.%u.%u\n", byte1, byte2, byte3, byte4);
+
+			printf("Key: Source Port: %u\n", key.src_port);
+			printf("Key: Destination Port: %u\n", key.dst_port);
+			printf("Key: Protocol: %u\n", key.protocol);
+            printf("Value: Counter: %u\n", value.counter);
+			printf("---------------\n");
+        }
+
+		printf("The map has %d elements\n", counter);
+		printf("******************************************************************************\n");
+        
+        sleep(3);
+
+        memset(&key, 0, sizeof(key));
+    
+
 	}
 
 	tc_opts.flags = tc_opts.prog_fd = tc_opts.prog_id = 0;
@@ -108,8 +162,6 @@ int main(int argc, char **argv)
 	}
 
 cleanup:
-	ring_buffer__free(rbuf);
-
 	if (hook_created)
 		bpf_tc_hook_destroy(&tc_hook);
 	tc_bpf__destroy(skel);

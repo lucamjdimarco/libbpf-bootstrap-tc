15d58c5b49b8f449b1fe11cab2e8e5a0d41957c0
DiTotto <luca.ditotto99@gmail.com>
2024-07-03 18:55:21 +0200

    .

--- src/c/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/totto/Desktop/libbpf-bootstrap-tc/src/c/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/totto/Desktop/libbpf-bootstrap-tc/bpftool/include/linux/compiler.h \
- /home/totto/Desktop/libbpf-bootstrap-tc/bpftool/include/linux/compiler_types.h \
- /home/totto/Desktop/libbpf-bootstrap-tc/bpftool/include/linux/compiler-gcc.h

a62333e7b7deeb288e963ee6808758c649c4471d
lucamjdimarco <lucadm24@gmail.com>
2024-07-09 15:53:50 +0200

    Test seg fault

--- src/c/tc.c
+++ src/c/tc.c
@@ -303,7 +303,7 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     printf("Event: ts=%llu flowid=%llu counter=%llu\n", event->ts, event->flowid, event->counter);
 
     // Write data to InfluxDB
-    int ret = write_data_influxdb(influx_handler, event->flowid, event->counter, event->ts);
+    int ret = write_data_influxdb(influx_handler, event->ts, event->flowid, event->counter);
     if (ret != 0) {
         fprintf(stderr, "Failed to write data to InfluxDB\n");
     }

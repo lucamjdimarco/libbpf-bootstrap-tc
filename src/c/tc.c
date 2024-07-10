18bcdfa38ae69c1ac28f3b96ba3bf0a0f5cf3f62
lucamjdimarco <lucadm24@gmail.com>
2024-07-10 17:27:50 +0200

    Fix

--- src/c/tc.c
+++ src/c/tc.c
@@ -307,13 +307,13 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 		fprintf(stderr, "Error: influx_handler is NULL\n");
 	}
 
-    printf("Event: ts=%llu flowid=%llu counter=%llu\n", event->ts, event->flowid, event->counter);
+    //printf("Event: ts=%llu flowid=%llu counter=%llu\n", event->ts, event->flowid, event->counter);
 
     // Write data to InfluxDB
-    int ret = write_data_influxdb(influx_handler, event->ts, event->flowid, event->counter);
-    if (ret != 0) {
-        fprintf(stderr, "Failed to write data to InfluxDB\n");
-    }
+    // int ret = write_data_influxdb(influx_handler, event->ts, event->flowid, event->counter);
+    // if (ret != 0) {
+    //     fprintf(stderr, "Failed to write data to InfluxDB\n");
+    // }
 
     return 0;
 }

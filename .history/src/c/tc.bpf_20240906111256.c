d7d1e0225900031dfc38e6c33c0465f0e616429e
lucamjdimarco <lucadm24@gmail.com>
2024-09-06 11:13:17 +0200

    Try to fix addr

--- .history/src/c/tc.bpf_20240906111256.c
+++ .history/src/c/tc.bpf_20240906111256.c
@@ -600,7 +600,7 @@ int tc_ingress(struct __sk_buff *ctx)
         case bpf_htons(ETH_P_IP): {
             struct packet_info new_info = {};
             classify_ipv4_packet(&new_info, data_end, data);
-            //CLASSIFY_PACKET_AND_UPDATE_MAP(map_ipv4, new_info, QUINTUPLA, ipv4_flow);
+            CLASSIFY_PACKET_AND_UPDATE_MAP(map_ipv4, new_info, QUINTUPLA, ipv4_flow);
             struct classify_packet_args args = {
                 .map_name = &map_ipv4,
                 .new_info = &new_info,

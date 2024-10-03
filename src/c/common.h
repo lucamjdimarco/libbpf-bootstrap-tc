07e6ea30a851d70a9e630444848193d4099c6ce3
DiTotto <luca.ditotto99@gmail.com>
2024-10-03 21:47:05 +0200

    fixing ipv6 filter

--- src/c/common.h
+++ src/c/common.h
@@ -3,6 +3,7 @@
 #define COMMON_HEADER_H
 
 #define TC_ACT_OK      0
+#define TC_ACT_SHOT    2
 #define ETH_P_IP       0x0800 /* Internet Protocol packet	*/
 #define ETH_P_IPV6     0x86DD /* IPv6 */
 #define ETH_P_8021Q    0x8100 /* 802.1Q VLAN Extended Header */

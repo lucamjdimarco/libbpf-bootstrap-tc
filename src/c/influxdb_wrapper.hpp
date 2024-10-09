168d0f58970dfcacdef54ce287be36dc48265250
DiTotto <luca.ditotto99@gmail.com>
2024-10-09 10:42:42 +0200

    fix

--- src/c/influxdb_wrapper.hpp
+++ src/c/influxdb_wrapper.hpp
@@ -12,6 +12,9 @@ public:
 	void showDatabases();
 	int writeTemperature(const char *city, double temp);
 	int writeData(uint64_t ts, uint64_t flowid, uint64_t counter);
+	int writeDataBatch(const std::vector<uint64_t>& timestamps,
+						const std::vector<uint64_t>& flowids,
+						const std::vector<uint64_t>& counters);
 	//void showData(const std::string& measurement);
 private:
 	std::unique_ptr<influxdb::InfluxDB> db;

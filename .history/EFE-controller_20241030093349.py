d49706097166fbfed180a33687faf0709e17fc62
lucamjdimarco <lucadm24@gmail.com>
2024-10-30 09:37:41 +0100

    Reinserimento bcc

--- .history/EFE-controller_20241030093349.py
+++ .history/EFE-controller_20241030093349.py
@@ -7,21 +7,24 @@ import os
 r = redis.Redis(host='redis', port=6379, db=0)
 machine_id = os.popen("cat /etc/machine-id").read().strip()
 
-# Carica e attacca il programma eBPF dal file sorgente
-b = BPF(src_file="tc.bpf.c")
+# Compila e carica il programma eBPF
+with open("src/c/tc.bpf.c", "r") as f:
+    bpf_code = f.read()
+    
+# Inizializza l'istanza eBPF
+b = EBPFWrapper(text=prog)
 
-# Attiva il programma eBPF per monitorare la syscall 'clone' e chiama la funzione definita in eBPF 'track_clone'
-b.attach_kprobe(event="clone", fn_name="track_clone")
+# Attiva il programma eBPF
+b.attach_kprobe(event='clone', fn_name='track_clone')
 
-# Inizializza la chiave machine_id in Redis con valore 0, se non esiste
+# Se non esiste la chiave machine_id in Redis, la inizializza con valore 0
 if r.get(machine_id) is None:
     r.set(machine_id, 0)
 
 # Funzione per recuperare l'ultimo flow_id dalla mappa eBPF
 def get_current_flow_id():
     try:
-        flow_map = b.get_table("flowpy_map")  # Accede alla mappa eBPF
-        value = flow_map[0].value  # Accede alla chiave fissa 0
+        value = b.get_map("flowpy_map").get(0)  # Accede alla chiave fissa 0 nella mappa
         return value
     except KeyError:
         # Se la chiave non esiste, restituisce None

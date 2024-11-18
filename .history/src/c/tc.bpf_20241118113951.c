02023585942cfa6c1985c4bb4759a85815192594
lucamjdimarco <lucadm24@gmail.com>
2024-11-18 11:41:00 +0100

    Try to pass interface and machine_id from user to kernel

--- .history/src/c/tc.bpf_20241118113951.c
+++ .history/src/c/tc.bpf_20241118113951.c
@@ -631,22 +631,18 @@ int tc_ingress(struct __sk_buff *ctx)
 
 	/* ---- */
 
-	if(isFirst == 0) {
-		u32 key = 0;
-		char *machine_id = bpf_map_lookup_elem(&map_start_value, &key);
-		if (machine_id == NULL) {
-			bpf_printk("Machine ID not found\n");
-			return TC_ACT_OK;
-		}
+	u32 key = 0;
+	char *machine_id = bpf_map_lookup_elem(&map_start_value, &key);
+	if (machine_id == NULL) {
+		bpf_printk("Machine ID not found\n");
+		return TC_ACT_OK;
+	}
 
-		key = 1;
-		char *interface = bpf_map_lookup_elem(&map_start_value, &key);
-		if (interface == NULL) {
-			bpf_printk("Interface not found\n");
-			return TC_ACT_OK;
-		}
-
-		isFirst = 1;
+	key = 1;
+	char *interface = bpf_map_lookup_elem(&map_start_value, &key);
+	if (interface == NULL) {
+		bpf_printk("Interface not found\n");
+		return TC_ACT_OK;
 	}
 
 	/* ---- */

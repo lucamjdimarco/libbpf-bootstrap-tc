a15ebc0888f2e949529636ca45a854ea3c115999
lucamjdimarco <lucadm24@gmail.com>
2024-12-06 11:38:32 +0100

    rename maps and fix build of flow_id

--- .history/src/c/tc.bpf_20241206113823.c
+++ .history/src/c/tc.bpf_20241206113823.c
@@ -346,7 +346,6 @@ static __always_inline int classify_packet_and_update_map(struct classify_packet
 
 		*new_flow_event = flow_id;
 		bpf_printk("Flow ID %llu sent to user-space\n", flow_id);
-		bpf_ringbuf_submit(new_flow_event, 0);
 
 		/* ---- */
 

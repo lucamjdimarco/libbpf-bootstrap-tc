57f3826e9317abd15a9c1529c8e3c1c4fd53e6ed
lucamjdimarco <lucadm24@gmail.com>
2025-01-15 15:40:51 +0100

    try to fix deamon

--- .history/eFFECTDaemon_20250115153621.py
+++ .history/eFFECTDaemon_20250115153621.py
@@ -27,21 +27,21 @@ def listen_to_redis():
     print("Listening for messages on channel 'command_channel'...")
 
     for message in pubsub.listen():
-        if message['type'] == 'message':
-            try:
-                data = str(message['data'])  # Conversione esplicita a stringa
-                print(f"Received message: {data}")
-                parts = data.split()
-                command = parts[0]
-                if command == "attach" and len(parts) == 4:
-                    interface = parts[1]
-                    protocol = parts[2]
-                    classifier = int(parts[3])  # Converti a intero
-                    main(interface, protocol, classifier)
-                else:
-                    print(f"Invalid command format: {data}")
-            except Exception as e:
-                print(f"Error processing message: {e}")
+    if message['type'] == 'message':
+        try:
+            data = str(message['data'])  # Conversione esplicita a stringa
+            print(f"Received message: {data}")
+            parts = data.split()
+            command = parts[0]
+            if command == "attach" and len(parts) == 4:
+                interface = parts[1]
+                protocol = parts[2]
+                classifier = int(parts[3])  # Converti a intero
+                main(interface, protocol, classifier)
+            else:
+                print(f"Invalid command format: {data}")
+        except Exception as e:
+            print(f"Error processing message: {e}")
 
 # Mount the bpf filesystem - Function passed from EFE-controller.py
 def mount_bpf(mount_point):
